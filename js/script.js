fa55971401cded3b501204635f2b93e6d81fbf45
artneto <arthur_neto@icloud.com>
2021-09-27 18:17:17 +0000

    add function player to X and O

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 const cell = document.querySelectorAll(".cell");
-console.log(cell);
+let checkTurn=true;
+const PLAYER_X="X";
+const PLAYER_O="O";
+
+document.addEventListener("click", (event) =>{
+    if(event.target.matches(".cell")){
+    play(event.target.id);
+    }
+
+});
+
+function play(id){
+    const cell = document.getElementById(id);
+    turn = checkTurn ? PLAYER_X : PLAYER_O;
+    cell.textContent = turn;
+    checkTurn=checkTurn;
+}

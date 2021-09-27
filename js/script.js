1a8f51e96d6db25c72fa19c7b9edd5f4d6984195
artneto <arthur_neto@icloud.com>
2021-09-27 18:39:43 +0000

    add all possible winning combination

--- js/script.js
+++ js/script.js
@@ -5,13 +5,24 @@ let checkTurn=true;
 const PLAYER_X="X";
 const PLAYER_O="O";
 
+const COMBINATION =  [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+
 document.addEventListener("click", (event) =>{
     if(event.target.matches(".cell")){
     play(event.target.id);
     }
 
 });
-
+//Function to check the turn from each player. Player X "true" is the first to play, Player O "false" is after Player X.
 function play(id){
     const cell = document.getElementById(id);
     turn = checkTurn ? PLAYER_X : PLAYER_O;
@@ -20,6 +31,9 @@ function play(id){
     checkWinner(turn);
 }
 
-function checkWinner() {
+//Game rules: who is the winner? // if there was a tie//if there is another round.
+
+function checkWinner(event){
+
 
 }
